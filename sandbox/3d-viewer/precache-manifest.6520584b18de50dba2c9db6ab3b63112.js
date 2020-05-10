f9b7c549e91c4147b95666b9587086e655374dac
Rufus31415 <androidflorent@gmail.com>
2020-05-10 17:47:20 +0200

    add 3d viewer

---
+++ sandbox/3d-viewer/precache-manifest.6520584b18de50dba2c9db6ab3b63112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e5b8159d74a818b774cff78b747726",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1f9e4e832899bb0b7580",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b0b18f18cf573c9bc081",
+    "url": "./static/js/2.4e68f9d7.chunk.js"
+  },
+  {
+    "revision": "4110c0f5a32c5069eab17a05c34fbdd4",
+    "url": "./static/js/2.4e68f9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9e4e832899bb0b7580",
+    "url": "./static/js/main.0bd5215b.chunk.js"
+  },
+  {
+    "revision": "b9aaa5d7f34616360b0a",
+    "url": "./static/js/runtime-main.92907802.js"
+  }
+]);
